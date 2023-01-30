72ae3fef42a80d11508531ed21935eb193df25d3
raphaeluziel <raphaeluziel@hotmail.com>
2023-01-30 16:53:09 -0500

    project 04

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(RESET_I)
+    // Set variable i to 8192, the LAST register of SCREEN map
+    @8192
+    D=A
+    @i
+    M=D
+
+(LISTEN)
+    // Listen to the keyboard, and if 0, no key pressed, jump to blank
+    @KBD
+    D=M
+    @BLANK
+    D;JEQ
+
+    // Key is pressed, get current memory register (i), then get location
+    // by adding it to SCREEN, then color it black by setting M=-1 (1111111111111111)
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=-1
+
+    // Skip the BLANK
+    @CONTINUE
+    0;JMP
+
+(BLANK)
+    // Key was not pressed so color the 16 pixels white (M=0)
+    @i
+    D=M
+    @SCREEN
+    A=D+A
+    M=0
+
+(CONTINUE)
+    // Reduce value of i by 1
+    @i
+    MD=M-1
+
+    // If i < 0, the entire screen has been painted, so reset i, and begin again
+    @RESET_I
+    D;JLT
+
+    // Listen for key presses again
+    @LISTEN
+    0;JMP
+    
+(END)
+    @END
+    0;JMP
+    
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,75 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+//  set a=0, b=0, product=0, ander=1    // ander checks if bit is 0 or 1
+//  
+//  ADD LOOP
+//  if (b & ander == 0)  // the bit is a 0
+//      do NOT add, go to SHIFT
+//  else
+//      add a to product (R2)
+//
+//  a = a + a       // double a to shift bits left
+//  if (a == 0)     // shifted all bits beyond 16
+//      END program
+//
+//  ander = ander + ander       // shift ander to next bit
+//  go to ADD LOOP
+
+    // Set a to R0 input
+    @R0
+    D=M
+    @a
+    M=D
+
+    // Set b to R1 input
+    @R1
+    D=M
+    @b
+    M=D
+
+    // Set product, R2 to 0
+    @R2
+    M=0
+
+    // Set ander to 1
+    @ander
+    M=1
+
+(ADD)
+    // Check if current bit place in b is 0 or 1
+    // If it's zero go to SHIFT, else add to product
+    @b
+    D=M
+    @ander
+    D=D&M
+    @SHIFT
+    D;JEQ
+
+    // The bit is 1 so add to product
+    @a
+    D=M
+    @R2
+    M=D+M
+
+(SHIFT)
+    // Shift a's bits to left one place (multiply by 2)
+    @a
+    D=M
+    MD=D+M
+
+    // If shifted all 1's out of the 16 bits, END program
+    @END
+    D;JEQ
+
+    // Shift ander's bits 1 place to left to check next bit
+    @ander
+    D=M
+    M=D+M
+
+    @ADD
+    0;JMP
+
+(END)
+    @END
+    0;JMP
